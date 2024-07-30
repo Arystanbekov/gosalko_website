1c0b9996a3abda322930830b8acd4e0aca72e831
Arystanbekov <ajtbekarystanbekov8@gmail.com>
2024-07-30 17:01:53 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const nav = document.querySelector('.nav_vkladok');
+    nav.classList.toggle('show');
+  });
+  
